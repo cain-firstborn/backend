b092c737cbdc6edb67cb02bc7730d364bce91f59
Bojan Stankovic <dev.bojan.stankovic@gmail.com>
2025-04-03 23:27:40 +0200

    Install and set up API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,8 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        health: '/up',
+        web      : __DIR__.'/../routes/web.php',
+        api      : __DIR__.'/../routes/api.php',
+        apiPrefix: '',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
